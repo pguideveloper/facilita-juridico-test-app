0fe9c326c3eddc4ed4d3a77719b23630af1a085b
pguideveloper <pedroguimaraes.contato@gmail.com>
2024-02-02 08:55:12 -0300

    feat: Axios, react-query and react-router-dom config and usage

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from './App';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <div>Error!</div>,
+    element: <App />,
+  },
+]);
